cd98cc1562000f189b5c6724840de0b842e18991
binarysudoku <binarysudoku@hotmail.co.uk>
2018-02-07 14:10:26 +0000

    flamethrower implementation and crystal cluster painting

---
+++ ScrapDroids_Main/Assets/Scripts/Weapons/FlameThrower/FlameThrower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrower : Weapon {
+
+	public GameObject projectile;
+
+	//vfx vars
+	public Transform muzzleFlashSocket;
+	public GameObject muzzleFlashFX;
+	public GameObject emptyFX;
+
+	private float spinUp;
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+	}
+
+	public override void Update () {
+		base.Update ();
+
+		if (!isEmpty && ammo <= 0) {
+			Instantiate (emptyFX, muzzleFlashSocket.position, muzzleFlashSocket.rotation * Quaternion.Euler(0,-90,0), null);
+			isEmpty = true;
+		}
+	}
+
+	public override void Fire() {
+		if (currentCoolDown <= 0) {
+			if (ammo > 0) {
+				ammo -= 1;
+
+				Instantiate (muzzleFlashFX, muzzleFlashSocket.position, muzzleFlashSocket.rotation, muzzleFlashSocket);
+
+				Instantiate (projectile, muzzleFlashSocket.position, muzzleFlashSocket.rotation);
+			}
+		}
+	}
+}
---
+++ ScrapDroids_Main/Assets/Scripts/Weapons/FlameThrower/FlameThrowerOil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrowerOil : Projectile {
+
+	public GameObject impactFX;
+	public Transform pool;
+
+	Rigidbody rb;
+
+	public Vector3 poolSize;
+
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody>();
+		rb.AddForce(gameObject.transform.forward * Random.Range(50000,100000) );
+		pool.localScale = gameObject.transform.lossyScale;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnCollisionEnter(Collision col) {
+		pool.localScale = poolSize;
+		Debug.Log(col.gameObject);
+	}
+	
+}
